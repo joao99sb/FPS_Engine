debe85ae95e753264f60c0a034a45a74c219c7e9
João Victor Dias <joaovictor665@gmail.com>
2021-03-01 18:00:22 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+
+#include <math.h>
+#include <windows.h>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+
+
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+double fFOV = 3.14159265358979 / 4.0;
+float fDepth = 16.0f;
+
+int main(int argc, char const *argv[]) {
+
+	// Create Screen buffer
+	wchar_t *screen = new wchar_t[nScreenHeight*nScreenWidth];
+
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+
+
+	DWORD dwBytesWritten = 0;
+
+	wstring map;
+	
+
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"######.........#";
+	map += L"#..............#";
+	map += L"#.#.......###..#";
+	map += L"#.#............#";
+	map += L"#.#............#";
+	map += L"#..####........#";
+	map += L"#......#.#.....#";
+	map += L"#......#.#.....#";
+	map += L"#.######.#.....#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+	auto tp1 = chrono::system_clock::now();
+	auto tp2 = chrono::system_clock::now();
+
+
+	// Game loop
+	while (1)
+	{
+
+		tp2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float fElapsedTime = elapsedTime.count() * 3 ; 
+		
+
+
+		//controls
+		//handle ccw rotation
+		if (GetAsyncKeyState((short)'A') & 0x8000) {
+			fPlayerA -= (1.0f) * fElapsedTime;
+		}
+		if (GetAsyncKeyState((short)'D') & 0x8000) {
+			fPlayerA += (1.0f) * fElapsedTime;
+		}
+		if (GetAsyncKeyState((short)'W') & 0x8000) {
+			fPlayerX += sinf(fPlayerA) * 1.0f * fElapsedTime;
+			fPlayerY += cosf(fPlayerA) * 1.0f * fElapsedTime;
+
+			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#')
+			{					
+				fPlayerX -= sinf(fPlayerA) * 1.0f * fElapsedTime;
+				fPlayerY -= cosf(fPlayerA) * 1.0f * fElapsedTime;
+			}
+		}
+		if (GetAsyncKeyState((short)'S') & 0x8000) {
+			fPlayerX -= sinf(fPlayerA) * 1.0f * fElapsedTime;
+			fPlayerY -= cosf(fPlayerA) * 1.0f * fElapsedTime;
+
+			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#')
+			{
+				fPlayerX += sinf(fPlayerA) * 1.0f * fElapsedTime;
+				fPlayerY += cosf(fPlayerA) * 1.0f * fElapsedTime;
+			}
+		}
+		//Exit the game
+		if (GetAsyncKeyState((short)'B') & 0x8000) {
+			break;
+		}
+
+
+		for (int x = 0; x < nScreenWidth; x++) {
+
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+			float fDistanceToWall = 0;
+
+
+			bool bHitWall = false;
+			bool bBoundary = false;
+
+			float fEyeX = sinf(fRayAngle);
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall)
+			{
+				fDistanceToWall += 0.1f;
+
+				int nTestX = int(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = int(fPlayerY + fEyeY * fDistanceToWall);
+
+
+				if (nTestX< 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
+					bHitWall = true;
+					fDistanceToWall = fDepth;
+				}
+				else {
+					if (map[nTestY * nMapWidth + nTestX] == '#') {
+						bHitWall = true;
+
+						vector<pair<float, float>> p; //distance, dot
+
+						for (int tx = 0; tx < 2; tx++)
+							for (int ty = 0; ty < 2; ty++)
+							{
+								float vy = (float)nTestY + ty - fPlayerY;
+								float vx = (float)nTestX + tx - fPlayerX;
+								float d = sqrt(vx*vx + vy*vy);
+								float dot = (fEyeX * vx / d) + (fEyeY * vy / d);
+								p.push_back(make_pair(d, dot));
+							}
+
+						sort(p.begin(), p.end(), [](const pair<float, float> &left, const pair<float, float> &right) {return left.first < right.first; });
+
+						float fBound = 0.01;
+						if (acos(p.at(0).second) < fBound) bBoundary = true;
+						if (acos(p.at(1).second) < fBound) bBoundary = true;
+
+					}
+				}
+			}
+
+			// calculate distance to ceiling and floor 
+			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;
+
+			short nShade = ' ';
+
+
+			if (fDistanceToWall <= fDepth / 4.0f) nShade = 0x2588;
+			else if (fDistanceToWall < fDepth / 3.0f) nShade = 0x2593;
+			else if (fDistanceToWall < fDepth / 2.0) nShade = 0x2592;
+			else if (fDistanceToWall < fDepth) nShade = 0x2591;
+			else nShade = ' ';
+
+			if (bBoundary) nShade = ' ';
+
+			for (int y = 0; y < nScreenHeight; y++)
+			{
+				if (y <= nCeiling)
+					screen[y*nScreenWidth + x] = ' ';
+				else if (y > nCeiling && y <= nFloor)
+					screen[y*nScreenWidth + x] = nShade;
+				else // Floor
+				{
+					// Shade floor based on distance
+					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
+					if (b < 0.25)		nShade = '#';
+					else if (b < 0.5)	nShade = 'x';
+					else if (b < 0.75)	nShade = '.';
+					else if (b < 0.9)	nShade = '-';
+					else nShade = ' ';
+
+					screen[y*nScreenWidth + x] = nShade;
+				}
+			}
+
+
+
+		}
+		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f", fPlayerX, fPlayerY, fPlayerA, 1.0f / fElapsedTime);
+
+		for (int nx = 0; nx < nMapWidth; nx++)
+		{
+			for (int ny = 0; ny < nMapWidth; ny++)
+			{
+				screen[(ny + 1)*nScreenWidth + nx] = map[ny * nMapWidth + nx];
+			}
+		}
+		screen[((int)fPlayerY + 1) * nScreenWidth + (int)fPlayerX] = 'P';
+
+
+
+
+
+		screen[nScreenHeight * nScreenWidth - 1] = '\0';
+		WriteConsole(hConsole, screen, nScreenHeight * nScreenWidth, &dwBytesWritten, NULL);
+
+	}
+
+
+	return 0;
+
+}
